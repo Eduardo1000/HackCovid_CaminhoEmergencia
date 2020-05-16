fa92ac41a9e3f1a59f9c1f6a63664c3851d96a83
Rafael Rocha <rocharhs@gmail.com>
2020-05-16 19:26:06 -0300

    notebooks de inspecao dos datasets

---
+++ notebooks/inspecao_dados/constants.py
@@ -0,0 +1,8 @@
+import os
+
+PATH_ROOT = "../../BASE_DE_DADOS_CNES_202003/dataset/"
+PATH_ESTABELECIMENTO = os.path.join(PATH_ROOT, "estabelecimentos.csv")
+PATH_UNIDADES = os.path.join(PATH_ROOT, "datasus.csv")
+PATH_LEITOS = os.path.join(PATH_ROOT, "leitos.csv")
+PATH_GRIPE = os.path.join(PATH_ROOT, "gripe.csv")
+
