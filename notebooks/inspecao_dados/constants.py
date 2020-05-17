8f6e032aba2fd6633a1d626b223a94f77d6fd160
Rafael Rocha <rocharhs@gmail.com>
2020-05-17 14:23:42 -0300

    continuando limpeza dos dados

--- notebooks/inspecao_dados/constants.py
+++ notebooks/inspecao_dados/constants.py
@@ -5,4 +5,5 @@ PATH_ESTABELECIMENTO = os.path.join(PATH_ROOT, "estabelecimentos.csv")
 PATH_UNIDADES = os.path.join(PATH_ROOT, "datasus.csv")
 PATH_LEITOS = os.path.join(PATH_ROOT, "leitos.csv")
 PATH_GRIPE = os.path.join(PATH_ROOT, "gripe.csv")
+PATH_GRIPE_MIN = os.path.join(PATH_ROOT, "gripe_min.csv")
 
